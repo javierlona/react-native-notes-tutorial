d4cf2ee642310bde0d33075389dad5898beccc78
Javier Lona <javierlona1@gmail.com>
2025-03-16 13:24:17 -0500

    Change index page to home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ff8c00" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontWeight: "bold", fontSize: 20 },
+        contentStyle: {
+          backgroundColor: "#fff",
+          paddingTop: 10,
+          paddingHorizontal: 10,
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
 }
